3ebc31af0158be809d5b7d28fcff23d0bc398321
夜渊 <yeyuan@wolfie.pet>
2026-07-30 22:27:27 +0800

    🐺 整体frame放大到90dp，狼崽45px居中，气泡有空间

--- app/src/main/java/com/wolfie/pet/OverlayService.kt
+++ app/src/main/java/com/wolfie/pet/OverlayService.kt
@@ -27,7 +27,7 @@ class OverlayService : Service() {
 
     private fun setupOverlay() {
         wm = getSystemService(WINDOW_SERVICE) as WindowManager
-        val size = dp(45)
+        val size = dp(90)
         params = WindowManager.LayoutParams(
             size, size,
             WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
